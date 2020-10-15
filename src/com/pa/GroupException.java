5e4019ba0511f22836a4567d49cd34081279890d
PedroAlexandrino <pedro.petran@gmail.com>
2020-10-15 14:59:35 +0100

    Initial commit

---
+++ src/com/pa/GroupException.java
@@ -0,0 +1,10 @@
+package com.pa;
+
+// Classe GroupException
+
+public class GroupException extends RuntimeException {
+
+    public GroupException(String message) {
+        super(message);
+    }
+}
---
+++ src/com/pa/Lab0Main.java
@@ -0,0 +1,28 @@
+package com.pa;
+import java.lang.*;
+import java.util.Scanner;
+
+public class Lab0Main {
+    public static void main(String[] args) {
+       Person person = new Person(10, "Pedro");
+       // Group group = new Group();
+    /*  Scanner texto = new Scanner(System.in);
+        int id;
+
+        String nome;
+        System.out.println("Id por favor:");
+        id = texto.nextInt();
+        System.out.println("Nome por favor:");
+        nome = texto.nextLine();
+        texto.close();
+        Person person = new Person(id,nome);
+
+       System.out.println(id);
+        System.out.println("Bem vindo" + nome);
+      */
+
+    }
+}
+
+
+
